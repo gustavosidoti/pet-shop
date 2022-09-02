2ce44957743e35d85721fe2ecede6d24b31ab37f
gustavosidoti <gustavosidoti@gmail.com>
2022-09-02 15:53:52 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// LOGICA
+
+function compra(precio) {
+
+    const descuento = 0.10;
+
+    let respuestaDesicion = prompt(`¿Posee cupón de descuento? - responder con SI o NO`);
+
+    if (respuestaDesicion === "SI") {
+        aplicaDescuento(precio);
+    } else {
+        finalizarCompra(precio);
+    }
+
+    function aplicaDescuento(precio) {
+        let contador = 0;
+        do {
+            let codigoDTO = prompt(`ingrese su código de descuento`);
+            if (codigoDTO === "DESCUENTO") {
+                let precioFinal = precio - (precio * descuento);
+                alert('Descuento del 10% aplicado correctamente');
+                finalizarCompra(precioFinal);
+                contador = 2;
+                break;
+            }
+
+            alert('Código de descuento no válido');
+            contador++;
+        } while (contador < 2);
+
+    }
+
+    function finalizarCompra(precio) {
+        alert(`Total de la compra ${precio}`);
+    }
+
+}
