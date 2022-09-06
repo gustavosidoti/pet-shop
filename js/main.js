2a726835e7e239e169fdee11963f106c631dc36e
gustavosidoti <gustavosidoti@gmail.com>
2022-09-05 21:36:02 -0300

    Incorporando Arrays

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 
-function compra(precio) {
+class Producto {
 
-    const descuento = 0.10;
-
-    let respuestaDesicion = prompt(`¿Posee cupón de descuento? - responder con SI o NO`);
-
-    if (respuestaDesicion === "SI") {
-        aplicaDescuento(precio);
-    } else {
-        finalizarCompra(precio);
-    }
-
-    function aplicaDescuento(precio) {
-        let contador = 0;
-        // CICLO
-        do {
-            let codigoDTO = prompt(`ingrese su código de descuento`);
-            if (codigoDTO === "DESCUENTO") {
-                let precioFinal = precio - (precio * descuento);
-                alert('Descuento del 10% aplicado correctamente');
-                finalizarCompra(precioFinal);
-                contador = 2;
-                break;
-            }
-
-            alert('Código de descuento no válido');
-            contador++;
-        } while (contador < 2);
-
-    }
-
-    function finalizarCompra(precio) {
-        alert(`Total de la compra ${precio}`);
+    constructor(nombre, precio, cantidad){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
     }
 
 }
+
+let arrayProductos = [];
+let comprobacion = "";
+let totalCompra = 0;
+
+do{
+    comprobacion = prompt('Ingrese nombre del producto comprado o fin para terminar de agregar productos al carro ');
+    if(comprobacion.toLowerCase() === "fin"){
+        break;
+    }else{
+        nombreP = comprobacion;
+        let precioP = prompt('Ingrese el precio del producto');
+        let cantidadP = prompt('Ingrese la cantidad del producto');
+        arrayProductos.push( new Producto (nombreP, precioP, cantidadP));
+    }
+
+}while(comprobacion.toLowerCase() != "fin");
+
+for (let producto of arrayProductos){
+    let totalProductoIndividual = 0;
+    totalProductoIndividual = producto.cantidad * producto.precio;
+    alert(`El producto agregado al carro es: ${producto.cantidad} unidades de ${producto.nombre} \ncon un precio por unidad de $ ${producto.precio}
+     \nTOTAL $ ${totalProductoIndividual}  `);
+     totalCompra += totalProductoIndividual;
+}
+
+alert(`TOTAL DE LA COMPRA $ ${totalCompra}`);
